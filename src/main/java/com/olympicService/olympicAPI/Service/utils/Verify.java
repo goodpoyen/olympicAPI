550f46633e4863a46cfc60a7053b02581c4988e2
poyen <goodpoyen@gamil.com>
2022-05-02 15:07:35 +0800

    verify method fix

--- src/main/java/com/olympicService/olympicAPI/Service/utils/Verify.java
+++ src/main/java/com/olympicService/olympicAPI/Service/utils/Verify.java
@@ -34,7 +34,7 @@ public class Verify {
 
 		dataValue = dataValue.toUpperCase();
 
-		if (dataValue.matches("[A-Z]\\d{4}")) {
+		if (dataValue.matches("[a-zA-Z][1-2]\\d{3}")) {
 			status = true;
 		}
 
@@ -57,10 +57,16 @@ public class Verify {
 		return status;
 	}
 
-	public Boolean checkDate(String date) {
+	public Boolean checkDate(String date, String dateFormat) {
 		Boolean status = false;
 
-		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		String[] buff = date.split("/");
+
+		if (buff.length > 2) {
+			return false;
+		}
+
+		SimpleDateFormat format = new SimpleDateFormat(dateFormat);
 
 		try {
 			format.setLenient(false);
@@ -76,6 +82,30 @@ public class Verify {
 	public Boolean checkSubject(String olympic) {
 		Boolean status = false;
 
+		if (olympic.contains("[TMO]奧林匹亞")) {
+			status = true;
+		}
+
+		if (olympic.contains("[IPHO]奧林匹亞")) {
+			status = true;
+		}
+
+		if (olympic.contains("[TWICHO]奧林匹亞")) {
+			status = true;
+		}
+
+		if (olympic.contains("[CTBO]奧林匹亞")) {
+			status = true;
+		}
+
+		if (olympic.contains("[IESO]奧林匹亞")) {
+			status = true;
+		}
+
+		if (olympic.contains("[TWIJSO]奧林匹亞")) {
+			status = true;
+		}
+
 		if (olympic.contains("[TOI]奧林匹亞")) {
 			status = true;
 		}
@@ -84,12 +114,111 @@ public class Verify {
 	}
 
 	public String checkOlympic(String olympic) {
+		if (olympic.contains("[TMO]")) {
+			return "TMO";
+		}
+
+		if (olympic.contains("[IPHO]")) {
+			return "IPHO";
+		}
+
+		if (olympic.contains("[TWICHO]")) {
+			return "TWICHO";
+		}
+
+		if (olympic.contains("[CTBO]")) {
+			return "CTBO";
+		}
+
+		if (olympic.contains("[IESO]")) {
+			return "IESO";
+		}
+
+		if (olympic.contains("[TWIJSO]")) {
+			return "TWIJSO";
+		}
 
 		if (olympic.contains("[TOI]")) {
 			return "TOI";
 		}
 
 		return "";
+	}
+
+	public Boolean checkMedical(String dataValue) {
+		Boolean status = false;
+
+		if (dataValue.contains("症")) {
+			status = false;
+		} else if (dataValue.contains("炎")) {
+			status = false;
+		} else if (dataValue.contains("病")) {
+			status = false;
+		} else if (dataValue.contains("傷")) {
+			status = false;
+		} else if (dataValue.contains("鬱")) {
+			status = false;
+		}
+
+//		else if (dataValue.contains("肝")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("膽")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("脾")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("胃")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("腎")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("腦")) {
+//			status = false;
+//		}
+
+//		else if (dataValue.contains("殘")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("障礙")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("身障")) {
+//			status = false;
+//		}
+
+//		else if (dataValue.contains("失調")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("過動")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("癲癇")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("妥瑞")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("自閉")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("亞斯")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("ADHD")) {
+//			status = false;
+//		}
+//		else if (dataValue.contains("adhd")) {
+//			status = false;
+//		}
+
+		else {
+			status = true;
+		}
+
+		return status;
 	}
 
 	public Boolean checkPassword(String password) {
